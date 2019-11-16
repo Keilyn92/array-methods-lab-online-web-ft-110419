05a534a5cbd54adf7936561b8e884903baa307c3
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-11-16 19:12:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,28 +1,6 @@
-
-dogs = ["pittbull","collie","mutt"]
-dog.include?("Bassett")
-
-
-      
-      
-cats_names = ["star","passion","lester"]
-cats_names.sort
-
-
- mike = ["adams"]
- mike.reverse
-
-
- cars = ["honda","mazda","lincoln"]
- cars.first
-
-
-cars = ["honda","mazda","lincoln"]
-cars.last
-
-size_of_balls = ["4","3","1"]
-size_of_balls> 5
-
+def using_include(array, element)
+  array.include?(element)
+end
 
 
 
